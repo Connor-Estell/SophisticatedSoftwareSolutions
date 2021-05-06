799b690b27d9fd139efdc0844e879b3e6acc1fad
samfish204 <64744508+samfish204@users.noreply.github.com>
2021-05-06 15:33:45 -0400

    Finished w comments

--- Price_Compare.java
+++ Price_Compare.java
@@ -1,5 +1,16 @@
 import java.util.Comparator;
 
+/*
+ * Name: Sophisticated Software Solutions
+ * Instructor: Dr Stephan
+ * Course: CSE 201, Section C
+ * Date: 5/6/2021
+ * Assignment: Appdex
+ * File: Price_Compare.java
+ * Description: This class compares the prices of app objects,
+ * and is used in the implementation of the sorting feature
+ * on the HomePage.
+ */
 public class Price_Compare implements Comparator<App>{
 
 	@Override
